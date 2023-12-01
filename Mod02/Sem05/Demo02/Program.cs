85e54fdd457e237a55079d7f18a6a99c8f7a12ce
Alexander Lazarenko <kerblif@unprism.ru>
2023-12-01 12:38:18 +0300

    Sem05 ToDo's

---
+++ Mod02/Sem05/Demo02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class MyFunction {
+    double xmi, xma;
+    public MyFunction(double mi, double ma)
+    {
+        xmi = mi; xma = ma;
+    }
+    public double this[double x] {
+        get {
+            return x < xmi | x > xma ? 0 :
+                Math.Sin(x);
+        }
+    }
+}
+class Program {
+    static void Main()
+    {
+        double rmi = -5, rma = 5;
+        MyFunction sin = new MyFunction(0, Math.PI);
+        double s = 0, del = 0.001;
+        for (double x = rmi; x < rma; x += del)
+            s += sin[x];
+        s *= del;
+        Console.WriteLine(s);
+    }
+}
